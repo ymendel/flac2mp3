c4970a959ad6aba39f5ea592026070603a6ba570
Yossef Mendelssohn <ymendel@pobox.com>
2007-12-06 22:43:44 -0600

    Added method to get output filename from input filename.

--- lib/flac2mp3.rb
+++ lib/flac2mp3.rb
@@ -5,6 +5,12 @@ module Flac2mp3
   class << self
     def convert(filename)
       raise TypeError unless FileTest.file?(filename)
+      filename.extend(Flac2mp3::StringExtensions)
+      output_filename(filename)
+    end
+    
+    def output_filename(filename)
+      filename.chomp('.flac') + '.mp3'
     end
   end
 end
--- spec/flac2mp3_spec.rb
+++ spec/flac2mp3_spec.rb
@@ -4,6 +4,10 @@ describe Flac2mp3 do
   it 'should convert' do
     Flac2mp3.should respond_to(:convert)
   end
+  
+  it 'should provide output filename' do
+    Flac2mp3.should respond_to(:output_filename)
+  end
 end
 
 describe Flac2mp3, 'when converting' do
@@ -28,8 +32,18 @@ describe Flac2mp3, 'when converting and given a filename belonging to a regular 
     FileTest.stubs(:file?).with(@filename).returns(true)
   end
   
-  it 'should check if the filename belongs to a regular file' do
+  it 'should not error' do
     lambda { Flac2mp3.convert(@filename) }.should_not raise_error(TypeError)
+  end
+  
+  it 'should extend the filename with the string extensions' do
+    @filename.expects(:extend).with(Flac2mp3::StringExtensions)
+    Flac2mp3.convert(@filename)
+  end
+  
+  it 'should get the output filename' do
+    Flac2mp3.expects(:output_filename).with(@filename)
+    Flac2mp3.convert(@filename)
   end
 end
 
@@ -41,5 +55,23 @@ describe Flac2mp3, 'when converting and given a filename not belonging to a regu
   
   it 'should error' do
     lambda { Flac2mp3.convert(@filename) }.should raise_error(TypeError)
+  end
+end
+
+describe Flac2mp3, 'when getting an output filename' do
+  it 'should require a filename' do
+    lambda { Flac2mp3.output_filename }.should raise_error(ArgumentError)
+  end
+  
+  it 'should accept a filename' do
+    lambda { Flac2mp3.output_filename('blah.flac') }.should_not raise_error(ArgumentError)
+  end
+  
+  it 'should convert a .flac extension to an .mp3 extension' do
+    Flac2mp3.output_filename('blah.flac').should == 'blah.mp3'
+  end
+  
+  it 'should append an .mp3 extension if no .flac extension exists' do
+    Flac2mp3.output_filename('blah').should == 'blah.mp3'
   end
 end
