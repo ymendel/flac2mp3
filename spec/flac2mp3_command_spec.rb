de08f60fdbc37ca3e700c8a5b603fa935f01fb7e
Yossef Mendelssohn <ymendel@pobox.com>
2008-06-25 17:19:12 -0500

    Added command-line option for deleting the original FLAC.

--- spec/flac2mp3_command_spec.rb
+++ spec/flac2mp3_command_spec.rb
@@ -32,7 +32,7 @@ describe 'flac2mp3 command' do
   end
   
   it 'should pass the filename to Flac2mp3 for conversion' do
-    Flac2mp3.expects(:convert).with('blah')
+    Flac2mp3.expects(:convert).with('blah', anything)
     run_command('blah')
   end
   
@@ -40,5 +40,25 @@ describe 'flac2mp3 command' do
     filename = 'blah'
     filename.expects(:dup).returns(filename)
     run_command(filename)
+  end
+  
+  it 'should pass on a true flac-deletion option if specified on the command line (using --delete)' do
+    Flac2mp3.expects(:convert).with(anything, true)
+    run_command('blah', '--delete')
+  end
+  
+  it 'should pass on a false flac-deletion option if specified on the command line (using --no-delete)' do
+    Flac2mp3.expects(:convert).with(anything, false)
+    run_command('blah', '--no-delete')
+  end
+  
+  it 'should pass on a true flac-deletion option if specified on the command line (using -d)' do
+    Flac2mp3.expects(:convert).with(anything, true)
+    run_command('blah', '-d')
+  end
+  
+  it 'should pass on a false flac-deletion option if nothing specified on the command line' do
+    Flac2mp3.expects(:convert).with(anything, false)
+    run_command('blah')
   end
 end
